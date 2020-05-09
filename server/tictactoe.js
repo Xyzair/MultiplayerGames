b3a9d84b3beac654ac4254bbefd9ad3f9971b0e5
Colby Biegler <xyzaire@gmail.com>
2020-05-08 20:04:38 -0700

    Backend properly acknowledges  turns, and updates the players properly

--- server/tictactoe.js
+++ server/tictactoe.js
@@ -2,12 +2,20 @@ class TicTacToe {
   constructor(p1, p2) {
     this._players = [p1, p2];
     this._grid = new Array(3);
+    this._turn = 0;
 
-    for (var i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
       this._grid[i] = [null, null, null];
     }
 
     this._sendToPlayers("Tic Tac Toe START!");
+
+    this._players.forEach((player, idx) => {
+      player.on("turn", (turn) => {
+        this._onTurn(idx, turn);
+        console.log("player: " + idx + " made move: " + turn);
+      });
+    });
   }
 
   _sendToPlayer(playerIndex, msg) {
@@ -18,14 +26,29 @@ class TicTacToe {
     this._players.forEach((p) => p.emit("message", msg));
   }
 
+  _sendUpdateToPlayers() {
+    this._players.forEach((p) => p.emit("update", this._grid));
+  }
+
   //turn is expected to be the array coords of the move
   _onTurn(playerIndex, turn) {
-    if (this._grid[turn[0]][turn[1]] === null) {
-      if (player === 0) {
-        this._grid[turn[0]][turn[1]] == "X";
-      } else {
-        this._grid[turn[0]][turn[1]] == "O";
+    //if it's your turn
+    if (this._turn === playerIndex) {
+      if (this._grid[turn[0]][turn[1]] === null) {
+        if (playerIndex === 0) {
+          this._grid[turn[0]][turn[1]] = "X";
+          this._sendToPlayer(playerIndex, `X placed at ${turn[0]}, ${turn[1]}`);
+          this._sendUpdateToPlayers();
+        } else {
+          this._grid[turn[0]][turn[1]] = "O";
+          this._sendToPlayer(playerIndex, `O placed at ${turn[0]}, ${turn[1]}`);
+          this._sendUpdateToPlayers();
+        }
+        this._turn = (playerIndex + 1) % 2;
       }
+    } else {
+      this._sendToPlayer(playerIndex, "It is not your turn");
+      console.log("player: " + playerIndex + " not your turn");
     }
   }
 }
