b0d4077e97abf26f46a4a64f61961daebf021270
Colby Biegler <xyzaire@gmail.com>
2020-05-08 17:31:28 -0700

    Add button listeners to the tic-tac-toe board and ran prettier

--- client/client.js
+++ client/client.js
@@ -1,38 +1,65 @@
 const writeEvent = (text) => {
-    //find event class on page
-    const parent = document.querySelector('#events');
-    //creates a new list element inside.
-    const element = document.createElement('li')
-    element.innerHTML = text;
+  //find event class on page
+  const parent = document.querySelector("#events");
+  //creates a new list element inside.
+  const element = document.createElement("li");
+  element.innerHTML = text;
 
-    parent.appendChild(element);
+  parent.appendChild(element);
 };
 
 const onFormSubmitted = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    //Find the chat box input
-    const input = document.querySelector('#chat');
-    //Store value from chat box.
-    const text = input.value;
+  //Find the chat box input
+  const input = document.querySelector("#chat");
+  //Store value from chat box.
+  const text = input.value;
 
-    //Clear input value on front end.
-    input.value = '';
-    console.log('text: ' + text);
+  //Clear input value on front end.
+  input.value = "";
+  console.log("entered text: " + text);
 
-    //Send message to server
-    sock.emit('message', text);
+  //Send message to server
+  sock.emit("message", text);
 };
 
-writeEvent("Page successfully fetch and Javascript successfully loaded");
+const addButtonListenersTTT = () => {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      document
+        .getElementById("ttt" + i + "-" + j)
+        .addEventListener("click", () => {
+          sock.emit("message", i + "-" + j);
+          console.log("Button pressed " + i + ", " + j);
+        });
+    }
+  }
+};
+
+writeEvent("Page successfully fetched and Javascript successfully loaded");
 
 //establishes connection with the server
 const sock = io();
 
-sock.on('message', (text) => {
-    writeEvent(text);
-})
+sock.on("message", (text) => {
+  writeEvent(text);
+});
 
 document
-    .querySelector('#chat-form')
-    .addEventListener('submit', onFormSubmitted);
+  .querySelector("#chat-form")
+  .addEventListener("submit", onFormSubmitted);
+
+// document.getElementById('ttt' + 2 +"-" + 2)
+// .addEventListener('click', () => {
+//     sock.emit('message', '2-2');
+//     console.log("Button pressed " + 2 + ", " + 2)
+// });
+
+// document.getElementById('ttt' + 0 +"-" + 1)
+// .addEventListener('click', () => {
+//     sock.emit('message', 0 +"-" + 1);
+//     console.log("Button pressed " + 0 + ", " + 1)
+// });
+
+addButtonListenersTTT();
